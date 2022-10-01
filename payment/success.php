adaa261204f67781fa2f69e30f037f9539c89c92
Hemant katurde <hemantkaturde123@gmail.com>
2022-10-01 16:47:24 +0530

    checking mail

--- payment/success.php
+++ payment/success.php
@@ -11,6 +11,65 @@ $sql = "UPDATE tbl_enquiry SET payment_status=1 WHERE enq_id=$id";
 
 if ($conn->query($sql) === TRUE) {
   //echo "Record updated successfully";
+
+// Recipient 
+$to = 'hemantkaturde123@gamil.com'; 
+ 
+// Sender 
+$from = 'hemantkaturde123@gamil.com'; 
+$fromName = 'hemant katurde'; 
+ 
+// Email subject 
+$subject = 'PHP Email with Attachment by CodexWorld';  
+ 
+// Attachment file 
+$file = "/codexworld.pdf"; 
+ 
+// Email body content 
+$htmlContent = ' 
+    <h3>PHP Email with Attachment by CodexWorld</h3> 
+    <p>This email is sent from the PHP script with attachment.</p> 
+'; 
+ 
+// Header for sender info 
+$headers = "From: $fromName"." <".$from.">"; 
+ 
+// Boundary  
+$semi_rand = md5(time());  
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
+ 
+// Headers for attachment  
+$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+ 
+// Multipart boundary  
+$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
+"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";  
+ 
+// Preparing attachment 
+if(!empty($file) > 0){ 
+    if(is_file($file)){ 
+        $message .= "--{$mime_boundary}\n"; 
+        $fp =    @fopen($file,"rb"); 
+        $data =  @fread($fp,filesize($file)); 
+ 
+        @fclose($fp); 
+        $data = chunk_split(base64_encode($data)); 
+        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" .  
+        "Content-Description: ".basename($file)."\n" . 
+        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" .  
+        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
+    } 
+} 
+$message .= "--{$mime_boundary}--"; 
+$returnpath = "-f" . $from; 
+ 
+// Send email 
+$mail = mail($to, $subject, $message, $headers, $returnpath);  
+ 
+// Email sending status 
+echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>"; 
+
+
 } else {
   //echo "Error updating record: " . $conn->error;
 }
@@ -40,7 +99,7 @@ if ($conn->query($sql) === TRUE) {
             <p><a class="" target="_blank"  href="<?php echo 'https://iictn.in/registration/paymentrecipt.php/'.$enq_id; ?>">Download Payment Receipt </a></p>
 
             <p><b>Addmission Form : </b></p>
-            <p><a class="" target="_blank"  href="<?php echo 'https://iictn.in/registration/new-registration-student.php/enq='.$enq_id; ?>">Admission Form Link </a></p>
+            <p><a class="" target="_blank"  href="<?php echo 'https://iictn.in/registration/new-registration-student.php?enq='.$enq_id; ?>">Admission Form Link </a></p>
 
             <p>Kindly contact your councillors for more Details</p>
             <!-- <p><a class="btn btn-warning" target="_blank" href="https://doctor.iictn.org/"> <br> -->
