1869278a339aec9f307477d038566b0edd27203d
Hemant katurde <hemantkaturde123@gmail.com>
2022-10-08 20:13:13 +0530

    code for added new changes

--- application/models/Event.php
+++ application/models/Event.php
@@ -9,7 +9,8 @@ class Event extends CI_Model{
     // for get all event date in one month
 	function getDateEvent($year, $month){
 		$year  = ($month < 10 && strlen($month) == 1) ? "$year-0$month" : "$year-$month";
-		$query = $this->db->select(TBL_TIMETABLE_TRANSECTIONS.'.date as event_date, count(time_table_id) as total_events ')->from(TBL_TIMETABLE)->join(TBL_TIMETABLE_TRANSECTIONS,TBL_TIMETABLE.'.id = '.TBL_TIMETABLE_TRANSECTIONS.'.time_table_id')->like(TBL_TIMETABLE_TRANSECTIONS.'.date', $year, 'after')->get();
+		$query = $this->db->select(TBL_TIMETABLE_TRANSECTIONS.'.date as event_date, count(time_table_id) as total_events ')->from(TBL_TIMETABLE)->join(TBL_TIMETABLE_TRANSECTIONS,TBL_TIMETABLE.'.id = '.TBL_TIMETABLE_TRANSECTIONS.'.time_table_id')->like(TBL_TIMETABLE_TRANSECTIONS.'.date', $year, 'after')->group_by(TBL_TIMETABLE_TRANSECTIONS.'.date')->get();
+	
 		if($query->num_rows() > 0){
 			$data = array();
 			foreach($query->result_array() as $row){
