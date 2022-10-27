059c691a217c0c4970bea8412432357a2ce50623
Hemant katurde <hemantkaturde123@gmail.com>
2022-10-27 23:58:35 +0530

    code for added new change

--- application/views/course/courseAttachment.php
+++ application/views/course/courseAttachment.php
@@ -2,18 +2,20 @@
     <div class="page-content fade-in-up col-md-10">
         <div class="ibox">
             <div class="ibox-head">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addCourseAttchment">
-                    <i class="fa fa-plus"></i> Add Topic / Chapter
-                </button>
-                <!-- <div class="ibox-tools"> -->
-                    <!-- <a class="btn btn-primary text-white" onclick="enquiry(0)" ><i class="fa fa-plus"></i> Add Enquiry</a> -->
-                    <!-- <a class="ibox-collapse"><i class="fa fa-minus"></i></a> -->
-                <!-- </div> -->
-              
+
+                <div>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addCourseAttchment">
+                        <i class="fa fa-plus"></i> Add Course Attachment
+                    </button>
+
+                    <button type="button" class="btn btn-primary">
+                        <a href="<?php echo base_url().'/courselisting';?>" style="color: black !important"><i class="fa fa-arrow-left"></i> Back</a>
+                     </button>
+                </div>
             </div>
             <div class="ibox-body">
               <div class="panel-body table-responsive">
-                <table id="view_coursetopicsattAchmentList" class="table table-bordered">
+                <table id="view_topic_attchemant" class="table table-bordered">
                     <thead>
                                 <tr>
                                     <th>Document Name</th>
