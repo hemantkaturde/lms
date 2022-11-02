d531e9549c89df5fd5043cdf2b94468e3bb176b3
Hemant katurde <hemantkaturde123@gmail.com>
2022-11-02 17:28:20 +0530

    code for addenes dvs

---
+++ test_email.php
@@ -0,0 +1,67 @@
+
+<?php
+
+   
+   
+    $to='hemantkaturde123@gmail.com';
+    $subject='Test'; 
+    $message='Tar';
+    $senderEmail='hemantkaturde123@gmail.com'; 
+    $senderName='HemaRT';
+    //$files = array();
+    $files = array( 
+        'https://iictn.in/markating_material/Doctors_Brochure.pdf', 
+        'https://iictn.in/markating_material/Non_Doctors_Brochure.pdf' 
+); 
+   
+   
+    // Sender info  
+    $from = $senderName." <".$senderEmail.">";  
+    $headers = "From: $from"; 
+ 
+    // Boundary  
+    $semi_rand = md5(time());  
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
+ 
+    // Headers for attachment  
+    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";  
+ 
+    // Multipart boundary  
+    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
+    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";  
+ 
+    // Preparing attachment 
+    if(!empty($files)){ 
+        for($i=0;$i<count($files);$i++){ 
+            if(is_file($files[$i])){ 
+                $file_name = basename($files[$i]); 
+                $file_size = filesize($files[$i]); 
+                 
+                $message .= "--{$mime_boundary}\n"; 
+                $fp =    @fopen($files[$i], "rb"); 
+                $data =  @fread($fp, $file_size); 
+                @fclose($fp); 
+                $data = chunk_split(base64_encode($data)); 
+                $message .= "Content-Type: application/octet-stream; name=\"".$file_name."\"\n" .  
+                "Content-Description: ".$file_name."\n" . 
+                "Content-Disposition: attachment;\n" . " filename=\"".$file_name."\"; size=".$file_size.";\n" .  
+                "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
+            } 
+        } 
+    } 
+     
+    $message .= "--{$mime_boundary}--"; 
+    $returnpath = "-f" . $senderEmail; 
+     
+    // Send email 
+    $mail = mail($to, $subject, $message, $headers, $returnpath);  
+     
+    // Return true if email sent, otherwise return false 
+    if($mail){ 
+        return true; 
+    }else{ 
+        return false; 
+    } 
+
+
+?>
