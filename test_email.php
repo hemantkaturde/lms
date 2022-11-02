ecb1f0615da7890095373cc660c7bb4d345b9fcc
Hemant katurde <hemantkaturde123@gmail.com>
2022-11-02 18:00:34 +0530

    code for addenes dvs

--- test_email.php
+++ test_email.php
@@ -1,52 +1,52 @@
 <?php
 
-// array with filenames to be sent as attachment
-    //$files = array("download (2).jpg","file2.pdf","file3.txt");
-    $files = array("download (2).jpg");
+ $name        = "IICTN";
+  $email       = "hemantkaturde123@gmail.com";
+  $to          = "hemantkaturde123@gmail.com";;
+  $from        = "hemantkaturde123@gmail.com";
+  $subject     = "IICTN - Admission Payment Recipt";
 
-    $files = array("http://localhost/lms_2/markating_material/Doctors_Brochure.pdf");
-    // email fields: to, from, subject, and so on
-    $to = "hemantkaturde123@gmail.com";
-    $from = "hemantkaturde123@mail.com"; 
-    $subject ="My subject"; 
-    $message = "My message";
-    $headers = "From: $from";
-    // boundary 
-    $semi_rand = md5(time()); 
-    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
-    // headers for attachment 
-    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" .   "
-                 boundary=\"{$mime_boundary}\""; 
-    // multipart boundary 
-    $message = "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n"
-               . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" . 
-                 "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n"; 
-    $message .= "--{$mime_boundary}\n";
-    // preparing attachments
-    for($x=0;$x< count($files);$x++){
-            $file = fopen($files[$x],"rb");
-            $data = fread($file,filesize($files[$x]));
-            fclose($file);
-            $data = chunk_split(base64_encode($data));
-            $message .= "Content-Type: {\"application/octet-stream\"};\n" .  
-                        " name=\"$files[$x]\"\n" . 
-            "Content-Disposition: attachment;\n" . " filename=\"$files[$x]\"\n" . 
-            "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-            $message .= "--{$mime_boundary}\n";
-    }
-    // send
-    $success = @mail($to, $subject, $message, $headers); 
-    if ($success) { 
-            echo "
- mail sent to $to ! 
 
-"; 
-    } else { 
-            echo "
- mail could not be sent! 
+  $fileatt     = "../invoices/PAYMENT_RECEIPT_1.pdf"; //file location
+  $fileatttype = "application/pdf";
+  $fileattname = "PAYMENT_RECEIPT_1.pdf"; //name that you want to use to send or you can use the same name
+  // $headers = "From: $from";
 
-"; 
-    } 
+  $header = "From: IICTN-Payment Receipt <enquiry@iictn.in> \r\n";
 
+  
+  // File
+  $file = fopen($fileatt, 'rb');
+  $data = fread($file, filesize($fileatt));
+  fclose($file);
+  
+  // This attaches the file
+  $semi_rand     = md5(time());
+  $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+  
+  
+  $headers .= "\nMIME-Version: 1.0\n" ."Content-Type: multipart/mixed;\n" ." boundary=\"{$mime_boundary}\"";
+
+  // $message = "This is a multi-part message in MIME format.\n\n" .
+    // "--{$mime_boundary}\n" .
+    // "Content-Type: text/plain; charset=\"iso-8859-1\n" .
+    // "Content-Transfer-Encoding: 7bit\n\n" .
+    // $mainMessage  . "\n\n";
+
+    // Multipart boundary  
+ $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" ."Content-Transfer-Encoding: 7bit\n\n" . $mainMessage . "\n\n";  
+  
+  $data = chunk_split(base64_encode($data));
+  $message .= "--{$mime_boundary}\n" ."Content-Type: {$fileatttype};\n" ." name=\"{$fileattname}\"\n" ."Content-Disposition: attachment;\n" ." filename=\"{$fileattname}\"\n" .
+  "Content-Transfer-Encoding: base64\n\n" .$data . "\n\n" ."--{$mime_boundary}--\n";
+
+  // Send the email
+  if(mail($to, $subject, $message, $headers)) {
+  
+    //echo "The email was sent.";
+  }
+  else {
+    //echo "There was an error sending the mail.";
+  }
 
 ?>
