16b273f33688d6a1e5397d598c639b899a19b7a1
Hemant katurde <hemantkaturde123@gmail.com>
2022-11-02 17:57:25 +0530

    code for addenes dvs

--- test_email.php
+++ test_email.php
@@ -1,70 +1,51 @@
-
 <?php
 
-   
-   
-    $to='hemantkaturde123@gmail.com';
-    $subject='Test'; 
-    $message='Tar';
-    $senderEmail='hemantkaturde123@gmail.com'; 
-    $senderName='HemaRT';
-    //$files = array();
-    $files = array( 
-        'http://localhost/lms_2/markating_material/Doctors_Brochure.pdf', 
-        'http://localhost/lms_2/markating_material/Non_Doctors_Brochure.pdf' 
-); 
+// array with filenames to be sent as attachment
+    //$files = array("download (2).jpg","file2.pdf","file3.txt");
+    $files = array("download (2).jpg");
 
+    $files = array("http://localhost/lms_2/markating_material/Doctors_Brochure.pdf");
+    // email fields: to, from, subject, and so on
+    $to = "hemantkaturde123@gmail.com";
+    $from = "hemantkaturde123@mail.com"; 
+    $subject ="My subject"; 
+    $message = "My message";
+    $headers = "From: $from";
+    // boundary 
+    $semi_rand = md5(time()); 
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+    // headers for attachment 
+    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" .   "
+                 boundary=\"{$mime_boundary}\""; 
+    // multipart boundary 
+    $message = "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n"
+               . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" . 
+                 "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n"; 
+    $message .= "--{$mime_boundary}\n";
+    // preparing attachments
+    for($x=0;$x< count($files);$x++){
+            $file = fopen($files[$x],"rb");
+            $data = fread($file,filesize($files[$x]));
+            fclose($file);
+            $data = chunk_split(base64_encode($data));
+            $message .= "Content-Type: {\"application/octet-stream\"};\n" .  
+                        " name=\"$files[$x]\"\n" . 
+            "Content-Disposition: attachment;\n" . " filename=\"$files[$x]\"\n" . 
+            "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+            $message .= "--{$mime_boundary}\n";
+    }
+    // send
+    $success = @mail($to, $subject, $message, $headers); 
+    if ($success) { 
+            echo "
+ mail sent to $to ! 
 
-    // Sender info  
-    $from = $senderName." <".$senderEmail.">";  
-    $headers = "From: $from"; 
- 
-    // Boundary  
-    $semi_rand = md5(time());  
-    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
- 
-    // Headers for attachment  
-    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";  
- 
-    // Multipart boundary  
-    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
-    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";  
- 
-    // Preparing attachment 
-    if(!empty($files)){ 
-        for($i=0;$i<count($files);$i++){ 
-            //if(is_file($files[$i])){ 
+"; 
+    } else { 
+            echo "
+ mail could not be sent! 
 
-            
-                $file_name = basename($files[$i]); 
-                $file_size = '80'; 
-                $message .= "--{$mime_boundary}\n"; 
-                $fp =    @fopen($files[$i], "rb"); 
-                $data =  @fread($fp, '80'); 
-                
-                @fclose($fp); 
-                $data = chunk_split(base64_encode($data)); 
-                $message .= "Content-Type: application/octet-stream; name=\"".$file_name."\"\n" .  
-                "Content-Description: ".$file_name."\n" . 
-                "Content-Disposition: attachment;\n" . " filename=\"".$file_name."\"; size=".$file_size.";\n" .  
-                "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
-           // } 
-        } 
-    } 
-
-    //exit;
-     
-    $message .= "--{$mime_boundary}--"; 
-    $returnpath = "-f" . $senderEmail; 
-     
-    // Send email 
-    $mail = mail($to, $subject, $message, $headers, $returnpath);  
-     
-    // Return true if email sent, otherwise return false 
-    if($mail){ 
-        return true; 
-    }else{ 
-        return false; 
+"; 
     } 
 
 
