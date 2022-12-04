dbf51d253fa3da87d9bb81d137026a49ec6a4336
Hemant katurde <hemantkaturde123@gmail.com>
2022-12-05 00:06:22 +0530

    code for all new changes

--- application/views/examination/view_questionpaper.php
+++ application/views/examination/view_questionpaper.php
@@ -188,7 +188,7 @@ $jsonstringtoArray = json_decode($access, true);
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header" style="background-color:#d2ae6d">
-                    <h5 class="modal-title" id="exampleModalLabel" style="color:#000">Uplaod Question Paper</h5>
+                    <h5 class="modal-title" id="exampleModalLabel" style="color:#000">Upload Question Paper</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
