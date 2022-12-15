bb15527364f618b287941174806d8c698db885b2
Hemant katurde <hemantkaturde123@gmail.com>
2022-12-15 21:02:17 +0530

    code for added new changes

--- application/views/student/student_dashboard.php
+++ application/views/student/student_dashboard.php
@@ -4,53 +4,40 @@
 <div class="content-wrapper">
     <!-- START PAGE CONTENT-->
     <div class="page-content fade-in-up">
-        <!-- <div class="row">
-            <div class="col-lg-3 col-md-6">
-                <div class="ibox bg-success color-white widget-stat">
-                    <div class="ibox-body banner-box">
-                        <h2 class="m-b-5 font-strong"><?php  ?></h2>
-                        <div class="m-b-5">SELECTED COURSES</div><i class="fa fa-book widget-stat-icon"></i>
-                         <div><i class="fa fa-level-up m-r-5"></i><small>25% higher</small></div>
-                        <div><i class="fa fa-level-up m-r-5"></i><small><a class="dashbord-short-link totalcourses"
-                                    href="<?php echo base_url().'courselisting'?>">Courses</a></small></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="ibox bg-info color-white widget-stat">
-                    <div class="ibox-body banner-box">
-                        <h2 class="m-b-5 font-strong"><?php ; ?></h2>
-                        <div class="m-b-5">ACTIVE COURSES</div><i class="fa fa-phone-square widget-stat-icon"></i>
-                         <div><i class="fa fa-level-up m-r-5"></i><small>17% higher</small></div> 
-                        <div><i class="fa fa-level-up m-r-5"></i><small><a class="dashbord-short-link totalenquiries"
-                                    href="<?php echo base_url().'enquirylisting'?>">ACTIVE COURSES</a></small></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="ibox bg-warning color-white widget-stat">
-                    <div class="ibox-body banner-box">
-                        <h2 class="m-b-5 font-strong"><?php ; ?></h2>
-                        <div class="m-b-5">CERTIFICATES</div><i class="fa fa-money widget-stat-icon"></i>
-                         <div><i class="fa fa-level-up m-r-5"></i><small>22% higher</small></div>
-                        <div><i class="fa fa-level-up m-r-5"></i><small><a class="dashbord-short-link totaladmissions"
-                                    href="<?php echo base_url().'enquirylisting'?>">Total Admissions</a></small></div>
 
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="ibox bg-danger color-white widget-stat">
-                    <div class="ibox-body banner-box">
-                        <h2 class="m-b-5 font-strong"><?php ; ?></h2>
-                        <div class="m-b-5">USERS</div><i class="fa fa-users widget-stat-icon"></i>
-                        <div><i class="fa fa-level-down m-r-5"></i><small>-12% Lower</small></div>
-                        <div><i class="fa fa-level-up m-r-5"></i><small><a class="dashbord-short-link totalusers"
-                                    href="<?php echo base_url().'enquirylisting'?>">Total Users</a></small></div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-     
+        <div class="col-lg-8 col-md-8">
+        <h5><b> Upcoming Topic Links</b></h5>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Course Name</th>
+                <th scope="col">Topic Name</th>
+                <th scope="col">Date</th>
+                <th scope="col">Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
     </div>
     <!-- END PAGE CONTENT-->
