6ecf53a3c9b925904b77e17a8064df887cec57f0
Hemant katurde <hemantkaturde123@gmail.com>
2022-12-15 14:09:48 +0530

    code for added new changes

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -68,10 +68,6 @@
         public function fetchBillinginfo(){
 
             $enq_id =  $this->session->userdata('enq_id');
-
-            print_r( $enq_id);
-            exit;
-
             $params = $_REQUEST;
             $totalRecords = $this->student_model->getTaxinvoicesCount($params,$enq_id);
             $queryRecords = $this->student_model->getTaxinvoices($params,$enq_id); 
