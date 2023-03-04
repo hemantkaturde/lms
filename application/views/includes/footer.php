7b6ccb1f20a25bb1917f7e935d3999858520d452
Hemant katurde <hemantkaturde123@gmail.com>
2023-03-04 17:10:26 +0530

    code for added Certificatitr

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -7,10 +7,10 @@
         </div>
     </div>
     <!-- BEGIN PAGA BACKDROPS-->
-    <div class="sidenav-backdrop backdrop"></div>
+    <!-- <div class="sidenav-backdrop backdrop"></div>
     <div class="preloader-backdrop">
         <div class="page-preloader">Loading</div>
-    </div>
+    </div> -->
     <!-- END PAGA BACKDROPS-->
     <!-- CORE PLUGINS-->
     <script src="<?php echo base_url(); ?>assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
