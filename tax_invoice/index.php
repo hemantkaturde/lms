5478d3469339072d608ecab3667afd98cc0df034
Hemant katurde <hemantkaturde123@gmail.com>
2023-04-20 17:02:09 +0530

    Code for added new changes

--- tax_invoice/index.php
+++ tax_invoice/index.php
@@ -3,6 +3,7 @@
    // include composer packages
    include "vendor/autoload.php";
    include "../db/config.php";
+   
 
    $enq_id = $_GET['enq_id'];
    $paymentid = $_GET['paymentid'];
@@ -205,6 +206,55 @@
         $pdf->SetFontSize('8'); // set font size
         $pdf->SetXY(162, 52); // set the position of the box
         $pdf->Cell(10, 196, $result_arry['prepared_by'], 0, 0, 'L'); // add the text, align to Center of cell
+
+
+
+        $number = $result_arry['totalAmount'];
+        $no = floor($number);
+        $point = round($number - $no, 2) * 100;
+        $hundred = null;
+        $digits_1 = strlen($no);
+        $i = 0;
+        $str = array();
+        $words = array('0' => '', '1' => 'One', '2' => 'Two',
+         '3' => 'Three', '4' => 'Four', '5' => 'Five', '6' => 'Six',
+         '7' => 'Seven', '8' => 'Eight', '9' => 'Nine',
+         '10' => 'Ten', '11' => 'Eleven', '12' => 'Twelve',
+         '13' => 'Thirteen', '14' => 'Fourteen',
+         '15' => 'Fifteen', '16' => 'Sixteen', '17' => 'Seventeen',
+         '18' => 'Eighteen', '19' =>'Nineteen', '20' => 'Twenty',
+         '30' => 'Thirty', '40' => 'Forty', '50' => 'Fifty',
+         '60' => 'Sixty', '70' => 'Seventy',
+         '80' => 'Eighty', '90' => 'Ninety');
+        $digits = array('', 'Hundred', 'Thousand', 'Lakh', 'Crore');
+        while ($i < $digits_1) {
+          $divider = ($i == 2) ? 10 : 100;
+          $number = floor($no % $divider);
+          $no = floor($no / $divider);
+          $i += ($divider == 10) ? 1 : 2;
+          if ($number) {
+             $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+             $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+             $str [] = ($number < 21) ? $words[$number] .
+                 " " . $digits[$counter] . $plural . " " . $hundred
+                 :
+                 $words[floor($number / 10) * 10]
+                 . " " . $words[$number % 10] . " "
+                 . $digits[$counter] . $plural . " " . $hundred;
+          } else $str[] = null;
+       }
+       $str = array_reverse($str);
+       $result = implode('', $str);
+       $points = ($point) ?
+         "." . $words[$point / 10] . " " . 
+               $words[$point = $point % 10] : '';
+       $words = $result . "Rupees Only";
+
+       //$words = $result . "Rupees  " . $points . " Paise";
+
+       $pdf->SetFontSize('8'); // set font size
+       $pdf->SetXY(43, 57); // set the position of the box
+       $pdf->Cell(10, 196, $words, 0, 0, 'L'); // add the text, align to Center of cell
 
 
      }
