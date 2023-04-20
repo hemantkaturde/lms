581349815b09f45389e50a68022c68075f17ac23
Hemant katurde <hemantkaturde123@gmail.com>
2023-04-20 20:59:29 +0530

    code for added new changes

--- payment/success.php
+++ payment/success.php
@@ -42,7 +42,7 @@ if ($conn->query($sql) === TRUE) {
 
 //   $Body  .= '<p> Step 6 - Received Certificate</p>';
 
-    $Body   = '<p><b>WELCOME TO IICTN</b></p>';
+    $Body   = '<p><b>WELCOME TO IICTN !! </b></p>';
 
     $Body  .='<p> We have received your Payment.</p>';
     $Body  .='<p> Transaction id: '.$row_last_payment['razorpay_payment_id'].'</p>';
@@ -71,7 +71,7 @@ if ($conn->query($sql) === TRUE) {
                 /* Send Whats App  Start Here */
                 $curl = curl_init();
                 $wp_url = 'https://iictn.in/tax_invoice/index.php?enq_id='.$row['enq_id'].'&paymentid='.$row_last_payment['id'];  
-                $text = 'WELCOME TO IICTN !!,  We have received your Payment. Transaction id :'.$row_last_payment['razorpay_payment_id'].$wp_url;
+                $text = 'WELCOME TO IICTN !!,  We have received your Payment. Transaction id :'.$row_last_payment['razorpay_payment_id'] .' Download Tax-Invoice Using Following Link '.$wp_url;
 
                 $mobile = '91'.$row['enq_mobile'];
                 $url = "https://marketing.intractly.com/api/send.php?number=".$mobile."&type=text&message=".urlencode($text)."&instance_id=643785A37C7FC&access_token=a78a6b9a06b1a24daa0d1402abd84d51";
@@ -86,28 +86,6 @@ if ($conn->query($sql) === TRUE) {
                 $response = curl_exec($ch);
                 curl_close($ch);
                 // echo $response;
-
-
-                $curl = curl_init();     
-                $wp_url = 'https://iictn.in/tax_invoice/index.php?enq_id='.$row['enq_id'].'&paymentid='.$row_last_payment['id'];  
-                
-                
-                $media ='We have attached the brochure and Syllabus for your reference, Feel free to contact us back, we will be delighted to assist and guide you. For more details you can also visit our website www.iictn.org';
-                $url_media = "https://marketing.intractly.com/api/send.php?number=".$mobile."&type=media&message=".urlencode($media)."&media_url=".$wp_url."&filename=Tax+Invoice&instance_id=643785A37C7FC&access_token=a78a6b9a06b1a24daa0d1402abd84d51";
-                
-
-                $ch_media = curl_init();
-                curl_setopt($ch_media, CURLOPT_URL, $url_media);
-                curl_setopt($ch_media, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch_media, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch_media, CURLOPT_FOLLOWLOCATION, true );
-                // This is what solved the issue (Accepting gzip encoding)
-                // curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");     
-                $response = curl_exec($ch_media);
-                curl_close($ch_media);
-                echo $response;
-
-                echo $url_media;
 
     }
 
