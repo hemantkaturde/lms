073791ce505a44672f7ad4458c71297d9c8fbe9d
Hemant katurde <hemantkaturde123@gmail.com>
2023-04-20 20:03:46 +0530

    COdd for added new changes

--- payment/success.php
+++ payment/success.php
@@ -42,10 +42,10 @@ if ($conn->query($sql) === TRUE) {
 
 //   $Body  .= '<p> Step 6 - Received Certificate</p>';
 
-    $Body   = 'WELCOME TO IICTN';
+    $Body   = '<p>WELCOME TO IICTN</p>';
 
-    $Body  .=' We have received your Payment.';
-    $Body  .=' Transaction id: '.$row_last_payment['razorpay_payment_id'];
+    $Body  .='<p> We have received your Payment.</p>';
+    $Body  .='<p> Transaction id: '.$row_last_payment['razorpay_payment_id'].'</p>';
 
 
     $Body  .= '<p>Click On the Below Link to fill Admission Form</p>';
@@ -54,13 +54,57 @@ if ($conn->query($sql) === TRUE) {
     $Body  .= '<p>Click to Download Payment Receipt</p>';
     $Body  .= '<p><B>Payment Receipt</B> : https://iictn.in/tax_invoice/index.php?enq_id='.$row['enq_id'].'&paymentid='.$row_last_payment['id'].'</p>';
 
+    $Body  .= '<p>For more details kindly contact your councillor or write us on admin@iictn.org</p>';
 
-  $header = "From: IICTN-Team <enquiry@iictn.in> \r\n";
-  //$header .= "Cc:ahemantkaturde123@gmail.com \r\n";
-  $header .= "MIME-Version: 1.0\r\n";
-  $header .= "Content-type: text/html\r\n";
+    $header = "From: IICTN-Team <enquiry@iictn.in> \r\n";
+    //$header .= "Cc:ahemantkaturde123@gmail.com \r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
 
-  $retval = mail($to,$Subject,$Body,$header);  
+    $retval = mail($to,$Subject,$Body,$header);  
+
+    if($retval){
+
+
+                /* Send Whats App  Start Here */
+                $curl = curl_init();
+                $text = 'WELCOME TO IICTN !!,  We have received your Payment. Transaction id :'.$row_last_payment['razorpay_payment_id'];
+
+                $mobile = '91'.$row['enq_mobile'];
+                $url = "https://marketing.intractly.com/api/send.php?number=".$mobile."&type=text&message=".urlencode($text)."&instance_id=".INSTANCE_ID."&access_token=".ACCESS_TOKEN."";
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+                // This is what solved the issue (Accepting gzip encoding)
+                // curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");     
+                $response = curl_exec($ch);
+                curl_close($ch);
+                // echo $response;
+
+
+                $curl = curl_init();     
+                $wp_url = "https://iictn.in/tax_invoice/index.php?enq_id=".$row['enq_id']."&paymentid=".$row_last_payment['id'];     
+                $media ='We have attached the brochure and Syllabus for your reference, Feel free to contact us back, we will be delighted to assist and guide you. For more details you can also visit our website www.iictn.org';
+                $url_media = "https://marketing.intractly.com/api/send.php?number=".$mobile."&type=media&message=".urlencode($media)."&media_url=".$wp_url."&filename=Tax+Invoice&instance_id=".INSTANCE_ID."&access_token=".ACCESS_TOKEN."";
+                
+                $ch_media = curl_init();
+                curl_setopt($ch_media, CURLOPT_URL, $url_media);
+                curl_setopt($ch_media, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch_media, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch_media, CURLOPT_FOLLOWLOCATION, true );
+                // This is what solved the issue (Accepting gzip encoding)
+                // curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");     
+                $response = curl_exec($ch_media);
+                curl_close($ch_media);
+                // echo $response
+
+
+
+
+    }
 
   
 }
