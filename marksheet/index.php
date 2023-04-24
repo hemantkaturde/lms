348d92d9623ffafa2a39c5ddf500c00cc4707328
Hemant katurde <hemantkaturde123@gmail.com>
2023-04-24 12:34:41 +0530

    Code for added new changes

--- marksheet/index.php
+++ marksheet/index.php
@@ -9,12 +9,14 @@ $student_id = $_GET['student_id'];
 
 
  /* get Student Details */
- $result = $conn->query("SELECT * from  tbl_users  
+ $result = $conn->query("SELECT *,tbl_examination.createdDtm as examdate from  tbl_users  
                          join tbl_student_answer_sheet on tbl_users.userid = tbl_student_answer_sheet.student_id
                          join tbl_course on tbl_student_answer_sheet.course_id = tbl_course.courseId
                          join tbl_course_type on tbl_course_type.ct_id = tbl_course.courseId
                          join tbl_users_enquires on tbl_users_enquires.user_id = tbl_users.userid
                          join tbl_admission on tbl_admission.enq_id = tbl_users_enquires.enq_id
+                         join tbl_examination on tbl_examination.course_id = tbl_course.courseId
+
                          where tbl_users.userid=$student_id and tbl_users.isDeleted=0 and tbl_users.user_flag='student' group by tbl_student_answer_sheet.student_id");
 
  $result_arry = $result->fetch_assoc();
@@ -22,6 +24,7 @@ $student_id = $_GET['student_id'];
  $course_name = $result_arry['course_name'];
  $dateofbirth = $result_arry['dateofbirth'];
  $mobile = $result_arry['mobile'];
+ $examdate = $result_arry['examdate'];
 
 
 
@@ -35,10 +38,9 @@ $pdf->AddPage();
 $pdf->useTemplate($tpl);
 
 $pdf->SetFont('Helvetica');
-
-$pdf->SetFontSize('10');
-$pdf->SetXY(52, 135);
-$pdf->Cell(0, 1,  $mobile.'/'.date("y",strtotime("-1 year")).'-'.date("y").'/WEB/MUM', 0, 0, 'L');
+$pdf->SetFontSize('10','B');
+$pdf->SetXY(52, 132);
+$pdf->Cell(0, 5,  $mobile.'/'.date("y",strtotime("-1 year")).'-'.date("y").'/WEB/MUM', 0, 0, 'L');
 
 
 $pdf->SetFontSize('10','B'); 
@@ -52,13 +54,30 @@ $pdf->Cell(0, 5,  $dateofbirth , 0, 0, 'L');
 
 
 
+$pdf->SetFontSize('10','B');
+$pdf->SetXY(168, 139.5);
+$new_date = date("Y-m-d",strtotime($examdate));
+$pdf->Cell(0, 5,  $new_date, 0, 0, 'L');
+
+
+$pdf->SetFontSize('10','B'); 
+$pdf->SetXY(52, 139.8);
+$pdf->Cell(0, 5,  $student_name, 0, 0, 'L');
+
+
+$pdf->SetFontSize('10','B'); 
+$pdf->SetXY(52, 146.8);
+$pdf->Cell(0, 5,  $dateofbirth , 0, 0, 'L');
+
+
 $pdf->SetFontSize('10','B'); 
 $pdf->SetXY(16, 176);
 $pdf->Cell(0, 10,  '1', 0, 0, 'L');
 
 $pdf->SetFontSize('10','B'); 
 $pdf->SetXY(25, 176);
-$pdf->Cell(0, 10,   $course_name, 0, 0, 'L');
+$pdf->Cell(0, 10,  $course_name, 0, 0, 'L');
+
 
 
 
