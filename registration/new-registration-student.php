4923f6f76ee3451251e17dac78cb94b3e53c0502
Hemant katurde <hemantkaturde123@gmail.com>
2023-04-29 17:51:03 +0530

    Code for added new changes

--- registration/new-registration-student.php
+++ registration/new-registration-student.php
@@ -183,7 +183,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' ) {
                                 </tr>
 
                                 <tr>
-                                    <td><b>User Name (First Name  or Email Id or Mobile Number )</b></td>
+                                    <td><b>User Name (First Name  or Email Id or Mobile Number)</b></td>
                                     <td>'.$username.' or '.$email.' or '.$mobile.'</td>
                                 </tr>
 
@@ -248,7 +248,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' ) {
                             
                                 /* Send Whats App  Start Here */
                             $curl = curl_init();
-                            $text = 'Login Link : https://www.iictn.in/ , Username (First Name  or Email Id or Mobile Number ) : '.$username .' Password :'.$main_pass;
+                            $text = 'Login Link : https://www.iictn.in/ , Username (First Name  or Email Id or Mobile Number) : '.$username .' Password :'.$main_pass;
                             //$text = 'Dear '.$enq_fullname.' Thank You for your interest in '.$all_course_name.', We have attached the brochure and Syllabus for your reference. Feel free to contact us back, we will be delighted to assist and guide you.For more details, you can also visit our website www.iictn.org';      
                             $mobile = '91'.$mobile;
                             $url = "https://marketing.intractly.com/api/send.php?number=".$mobile."&type=text&message=".urlencode($text)."&instance_id=643785A37C7FC&access_token=a78a6b9a06b1a24daa0d1402abd84d51";
