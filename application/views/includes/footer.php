e174b81def6df080c55f2d87a8f732a6ad1ac7f2
Hemant katurde <hemantkaturde123@gmail.com>
2023-08-19 17:07:42 +0530

    code for added new changes

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,7 @@
          
             <footer class="page-footer">
-                <div class="font-13">2022 © <b>IICTN</b> - All rights reserved.</div>
-                <a class="px-4" href="https://doctor.iictn.org/" target="_blank">IICTN</a>
+                <div class="font-13">2023 © <b>IICTN</b> - All rights reserved.</div>
+                <a class="px-4" href="https://doctor.iictn.org/" target="_blank"><b>IICTN</b></a>
                 <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
             </footer>
         </div>
