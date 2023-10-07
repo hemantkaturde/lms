eaf1fd35182dad0e88c0e6b8967b891e6bd2bdbe
Hemant katurde <hemantkaturde123@gmail.com>
2023-10-07 13:12:44 +0530

    Set Defulat Image In student login

--- application/views/course/addbackuptrainer.php
+++ application/views/course/addbackuptrainer.php
@@ -13,7 +13,7 @@ $jsonstringtoArray = json_decode($access, true);
                     </button>
                 </div>
                 <div class="ibox-title">     
-                   Date : <?=$getTopicinfo[0]->date?>
+                   Date : <?=$getCourseinfo[0]->date?>
                   <b>Course Name : </b> <?=$getCourseinfo[0]->course_name;?> 
                 | <b>Topic Name : </b> <?=$getTopicinfo[0]->topic;?> | Add Backup Trainer</div>
             </div>
