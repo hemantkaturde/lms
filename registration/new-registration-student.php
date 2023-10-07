e1e6faf9cd5740c1c2fff518238deaf56c93b2c1
Hemant katurde <hemantkaturde123@gmail.com>
2023-10-07 09:36:32 +0530

    code for added new changes

--- registration/new-registration-student.php
+++ registration/new-registration-student.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require '/vendor/autoload.php';
+require '../vendor/autoload.php';
 
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
 $url = "https://";   
