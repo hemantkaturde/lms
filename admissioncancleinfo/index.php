280be1335efbeeed731ed609a7818dd5c0a83543
Hemant katurde <hemantkaturde123@gmail.com>
2023-12-20 14:12:30 +0530

    code for added new changes

--- admissioncancleinfo/index.php
+++ admissioncancleinfo/index.php
@@ -7,23 +7,24 @@ include "../db/config.php";
 
  $admission_id = $_GET['admission_id'];
 
- $result = $conn->query("SELECT * from  tbl_admission where tbl_admission.id=$admission_id");
+ $result = $conn->query("SELECT tbl_admission.name,tbl_admission.lastname,tbl_admission.email,tbl_admission.mobile,sum(tbl_payment_transaction.totalAmount) as totalpaidAmount,tbl_payment_transaction.payment_date,tbl_payment_transaction.payment_mode,tbl_payment_transaction.datetime,tbl_enquiry.createdDtm,tbl_users.name as counsellor from  tbl_admission 
+  join tbl_enquiry on tbl_enquiry.enq_id = tbl_admission.enq_id 
+  join tbl_payment_transaction on tbl_payment_transaction.enquiry_id =tbl_enquiry.enq_id
+  left join tbl_users on tbl_admission.counsellor_name = tbl_users.userId
+  where tbl_admission.id=$admission_id");
 
  $result_arry = $result->fetch_assoc();
  $student_name = $result_arry['name'].' '.$result_arry['lastname'];
-//  $course_name = $result_arry['course_name'];
-//  $profile_pic = $result_arry['profile_pic'];
+ $adhar_number = '1251254556555125';
+ $email = $result_arry['email'];
+ $mobile = $result_arry['mobile'];
+ $totalpaidAmount = $result_arry['totalpaidAmount'];
 
-
-//  $mobile = $result_arry['mobile'];
-//  $evbtr = $result_arry['evbtr'];
-
-//  $admision_date = $result_arry['createdDtm'];
-
-//  $ct_name = $result_arry['ct_name'];
-
-//  $userid=$result_arry['userId'];
-
+ $payment_date = $result_arry['datetime'];
+ $payment_mode = $result_arry['payment_mode'];
+ $createdDtm = $result_arry['createdDtm'];
+ $counsellor = $result_arry['counsellor'];
+ 
  
 // Create new Landscape PDF
 // $pdf = new FPDI('l');
@@ -45,86 +46,51 @@ $pdf->useTemplate($tpl);
 $pdf->SetFont('Helvetica');
 
 // adding a Cell using:
-// $pdf->Cell( $width, $height, $text, $border, $fill, $align);
+//$pdf->Cell( $width, $height, $text, $border, $fill, $align);
+
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 47);
+$pdf->Cell(80, 8,  $student_name, 0, 0, 'L');
 
 
-
-// // First box - the user's Name
-// $pdf->SetFontSize('25','B'); // set font size
-// $pdf->SetXY(10, 162); // set the position of the box
-// $pdf->Cell(0, 10,  $student_name, 0, 0, 'C'); // add the text, align to Center of cell
-
-// First box - the user's Name
-// $pdf->SetFontSize('10'); // set font size
-// $pdf->SetXY(110, 205); // set the position of the box
-// $pdf->Cell(0, 1,  $evbtr, 0, 0, 'C'); // add the text, align to Center of cell
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 51.5);
+$pdf->Cell(80, 8, $adhar_number, 0, 0, 'L');
 
 
-// add the reason for certificate
-// note the reduction in font and different box position
-
-// $pdf->SetFontSize('30');
-// $pdf->SetXY(10, 133);
-// $pdf->MultiCell(190,11,$course_name, '0', 'C', 0);
-
-// // the day
-// $pdf->SetFontSize('20');
-// $pdf->SetXY(165.50,232);
-// $pdf->Cell(20, 10, date('d'), 0, 0, 'C');
-
-// $getStudentadmissionDetails = "SELECT tbl_admission.document_1 FROM tbl_users_enquires join tbl_admission on tbl_users_enquires.enq_id= tbl_admission.enq_id where tbl_users_enquires.user_id=$userid" ;
-// $resultStudentadmissionDetails = $conn->query($getStudentadmissionDetails);
-// $rowDataStudentadminssiondetails = $resultStudentadmissionDetails->fetch_assoc();
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 55.5);
+$pdf->Cell(80, 8, $email, 0, 0, 'L');
 
 
-// $profile_pic_admission = $rowDataStudentadminssiondetails['document_1'];
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 60);
+$pdf->Cell(80, 8, $mobile, 0, 0, 'L');
 
-// if($profile_pic_admission){
-//     $profile_pic_img = "../uploads/admission/".$profile_pic_admission;
-
-//     if(file_exists($profile_pic_img)) 
-//     {
-//         $pdf->Cell(20, 10, $pdf->Image($profile_pic_img, $pdf->GetX(), $pdf->GetY(), 25.00,33.00), 0, 0, 'L', false );
-//     }   
-// }
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 64.3);
+$pdf->Cell(80, 8,  $totalpaidAmount, 0, 0, 'L');
 
 
-// if($_SERVER['HTTP_HOST']=='localhost'){
-//     $base  = "http://".$_SERVER['HTTP_HOST'];
-//     $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-
-// }else{
-//     $base  = "https://".$_SERVER['HTTP_HOST'];
-//     $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-
-// }
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 68.5);
+$pdf->Cell(80, 8, $payment_date, 0, 0, 'L');
 
 
-// $pdf->SetFontSize('20');
-// $pdf->SetXY(15,232);
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 72.5);
+$pdf->Cell(80, 8, $payment_mode, 0, 0, 'L');
 
 
-// $pdf->SetFontSize('10');
-// $pdf->SetXY(42,270.2);
-// $pdf->Cell(0, 1,  $admision_date, 0, 0, 'L');
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 98);
+$pdf->Cell(80, 8, $createdDtm, 0, 0, 'L');
 
 
-// $pdf->SetFontSize('10');
-// $pdf->SetXY(42,275.6);
-// $pdf->Cell(0, 1,  date('Y-m-d H:i:s'), 0, 0, 'L');
+$pdf->SetFontSize('8');
+$pdf->SetXY(85, 102);
+$pdf->Cell(80, 8, $counsellor, 0, 0, 'L');
 
-// $pdf->SetFontSize('10');
-// $pdf->SetXY(145,275.6);
-// $pdf->Cell(0, 1,  $mobile.'/'.date("y",strtotime("-1 year")).'-'.date("y").'/WEB/MUM', 0, 0, 'L');
-
-
-// // the month
-// $pdf->SetXY(160,122);
-// $pdf->Cell(30, 10, date('M'), 0, 0, 'C');
-
-// // the year, aligned to Left
-// $pdf->SetXY(200,122);
-// $pdf->Cell(20, 10, date('y'), 0, 0, 'L');
 
 // render PDF to browser
 $pdf->Output();
