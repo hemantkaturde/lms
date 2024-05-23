2cbb743099a8646d875551128ae237c213405d91
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-23 20:40:29 +0530

    code for aaded new changes

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -2119,7 +2119,7 @@ public function getstudentinformation($student_id){
 public function getstudentcourselist($student_name){
 
     $this->db->select('enq_course_id');
-    $this->db->join(TBL_ENQUIRY, TBL_ENQUIRY.'.enq_number = '.TBL_USERS_ENQUIRES.'.enq_id');
+    $this->db->join(TBL_ENQUIRY, TBL_ENQUIRY.'.enq_id = '.TBL_USERS_ENQUIRES.'.enq_id');
     $this->db->where(TBL_USERS_ENQUIRES.'.user_id', $student_name);
     $query = $this->db->get(TBL_USERS_ENQUIRES);
     $fetch_result_enquiry_courses = $query->result_array();
