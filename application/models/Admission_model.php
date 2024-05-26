693e6885e57a7afc3d9b8c5234df912e6c60312a
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-26 10:11:55 +0530

    code for added new changs

--- application/models/Admission_model.php
+++ application/models/Admission_model.php
@@ -578,7 +578,7 @@ class Admission_model extends CI_Model
         $this->db->from('tbl_examination as BaseTbl');
         $this->db->join('tbl_course as course', 'course.courseId = BaseTbl.course_id');
         $this->db->join('tbl_timetable', 'tbl_timetable.course_id = course.courseId');
-        //$this->db->join('tbl_users as users', 'users.userId = timetable.trainer_id');
+        $this->db->join('tbl_users', 'tbl_users.userId = tbl_timetable.trainer_id');
         
         
         if($params['search']['value'] != "") 
