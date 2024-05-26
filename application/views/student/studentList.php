d490960a85ed5848b3e51eb6098bcad57fff7e60
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-26 09:44:52 +0530

    code for added new changs

--- application/views/student/studentList.php
+++ application/views/student/studentList.php
@@ -18,7 +18,7 @@
                                 <th>Name</th>
                                 <th>Mobile / Roll Number</th>
                                 <th>E mail</th>
-                                <th>Book Issued</th>
+                                <th>Course Books Issued to Student</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
