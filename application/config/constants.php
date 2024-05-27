a6b9cda8e5b8abedd8a6112e7b9d050a6d6c45f5
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-27 17:29:41 +0530

    code for added neew changes"

--- application/config/constants.php
+++ application/config/constants.php
@@ -143,10 +143,10 @@ define('DATEANDTIME', Date('Y-m-d H:i:s'));
 // define('EMAIL_NAME','IICTN');	
 
 
-define('EMAIL_SMTP_HOST','smtp.iictn.com');		// your smtp host e.g. smtp.gmail.com
+define('EMAIL_SMTP_HOST','mail.iictn.in');		// your smtp host e.g. smtp.gmail.com
 define('EMAIL_SMTP_AUTH','true');		// your smtp host e.g. smtp.gmail.com
 define('EMAIL_USERNAME','admin@iictn.in');	// Your system name
-define('EMAIL_PASSWORD','QY9ZT(!N#.Ro');	// Your email password
+define('EMAIL_PASSWORD','YanY).MeOD&M');	// Your email password
 define('EMAIL_SMTP_PORT','587');				    // mail, sendmail, smtp
 define('FROM_EMAIL','admin@iictn.in');		// e.g. email@example.com
 define('FROM_EMAIL_NAME','IICTN-Payment');
