d36e408117f0023f95db29b49d72fc8224eca3e0
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-27 17:32:03 +0530

    code for added neew changes"

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -1096,7 +1096,7 @@
                     //$retval = mail($to,$subject,$htmlContent,$header);
                     $retval =  sendmail($to,$subject,$body,$email_name,$attachmentList="");
                     //$retval =  1;
-                    $retval =  1;
+                    //$retval =  1;
                     if($retval){
 
                         /*Welcome Notification on whatsapp*/
