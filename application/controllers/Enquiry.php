9a9e5cfcbe5fa4b97543a945b88fe504586e2e3b
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-28 22:09:17 +0530

    code for added new changes

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -526,8 +526,8 @@
                         $header .= "MIME-Version: 1.0\r\n";
                         $header .= "Content-type: text/html\r\n";
                         
-                        $retval = mail($to,$Subject,$Body,$header);
-                        //$retval =  sendmail($to,$Subject,$Body,$email_name,$attachmentList="");
+                        //$retval = mail($to,$Subject,$Body,$header);
+                        $retval =  sendmail($to,$Subject,$Body,$email_name,$attachmentList="");
                     if($retval){
 
 
