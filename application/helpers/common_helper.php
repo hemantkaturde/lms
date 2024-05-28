9bd0c45a3826398f9f8e160ef3bf51a10633d5b4
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-28 20:01:53 +0530

    code for added new changes

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -436,13 +436,13 @@ if(!function_exists(('sendwhatsapp')))
                 
                 
                     $ch = curl_init();
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    //curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+                  //  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                   // curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
                     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($jsonData));
                     curl_setopt($ch, CURLOPT_URL, $url);
-                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
-                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+                    //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+                    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
                     $result = curl_exec($ch);
                     curl_close($ch);
                     echo $result;
