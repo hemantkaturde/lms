542e70e0f3e8bf3c48e0d73d5f92cc666d981eff
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-21 15:48:43 +0530

    codde for added  new changes

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -1126,7 +1126,7 @@ class Api_model extends CI_Model
     public function getcertificatetypelist(){
 
         $this->db->select('ct_id,ct_name');
-        $this->db->where(TBL_COURSE_TYPE.'.status',0);
+        $this->db->where(TBL_COURSE_TYPE.'.ct_status',0);
         $query = $this->db->get(TBL_COURSE_TYPE);
         return $query->result_array();
 
