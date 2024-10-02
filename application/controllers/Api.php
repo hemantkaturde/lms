8b0b85af28f087b76950c2e414f3c3a57869925a
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-02 15:02:31 +0530

    code for added new changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2806,7 +2806,7 @@ class Api extends BaseController
 
     /*Version API*/
     public function checkappversion(){
-         $responseData = array('android_version'=>'1.7','ios_version'=>'1.3');
+         $responseData = array('android_version'=>'1.8','ios_version'=>'1.3');
          setContentLength($responseData);
     }
 
