deeb68234bef23a4fa51e39b411876786dbf7bf7
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-02 15:43:41 +0530

    code for added new changes

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -642,8 +642,8 @@ class Api_model extends CI_Model
             $dataMaincourse['total_pending'] = 0;
         }
 
-        //$dataMaincourse['total_pending_amt'] = $dataMaincourse['total_pending'] - $dataMaincourse['total_revenue'];
-        $dataMaincourse['total_pending_amt'] = 20;
+        $dataMaincourse['total_pending_amt'] = $dataMaincourse['total_pending'] - $dataMaincourse['total_revenue'];
+        //$dataMaincourse['total_pending_amt'] = 20;
 
 
 
