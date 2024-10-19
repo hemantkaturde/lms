dbc3c04fc2269adbf057efea9269a9514d4ab652
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-19 17:53:50 +0530

    code for added new changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -213,6 +213,9 @@ class Api extends BaseController
 			$message = 'Validation error';
 			$data = array('userid' =>strip_tags(form_error('userid')),'user_flag' =>strip_tags(form_error('user_flag')));
 		}else{
+
+            $data = array('userid' =>trim($this->input->post('userid')),'user_flag' =>trim($this->input->post('user_flag')));
+
             $equiry_data = $this->Api_model->getEnquiryData($data);
             if($equiry_data){
                 $status = 'Success';
