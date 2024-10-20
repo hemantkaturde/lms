b744454c02323cd5a0645c12b8ddbc706e539cb4
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-20 08:19:04 +0530

    code for added new changes

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -1241,11 +1241,8 @@ class Api_model extends CI_Model
                 $data[$counter]['payment_mode'] =$value['payment_mode'];
                 $data[$counter]['status'] =$status;
 
-                $data[$counter]['action'] = '';
-                //$data[$counter]['action'] .= "<a style='cursor: pointer;'  href='tax_invoice/index.php?enq_id=".$value['enq_id']."&paymentid=".$value['paymentid']."' target='_blank'  class='print_tax_invoices' data-id='".$value['id']."'><img width='20' src=".ICONPATH."/print.png alt='Print Invoice' title='Print Invoice'></a> ";
-            
-                $data[$counter]['action'] .= "<a style='cursor: pointer;'  href='../tax_invoice/index.php?enq_id=".$value['enquiry_id']."&paymentid=".$value['paymentid']."' target='_blank'  class='print_tax_invoices' data-id='".$value['id']."'><img width='20' src=".ICONPATH."/print.png alt='Print Invoice' title='Print Invoice'></a> ";
-               
+                $data[$counter]['invoice_link'] = "https://iictn.in/tax_invoice/index.php?enq_id=".$value['enquiry_id']."&paymentid=".$value['paymentid'];
+                
              
                 $counter++; 
             }
