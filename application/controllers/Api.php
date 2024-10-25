88549961a4966c195671df630a70322a97a049f3
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-25 20:18:58 +0530

    code for added new changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -3179,11 +3179,18 @@ class Api extends BaseController
 
             $data['get_student_enquiry_id'] = $this->admission_model->getstudentEenquiryid($userId)[0];
             $followDataenquiry= $this->enquiry_model->getEnquiryInfo($data['get_student_enquiry_id']['enq_id'])[0];
-            $data['getEnquirypaymentInfo'] = $this->enquiry_model->getEnquirypaymentInfo($data['get_student_enquiry_id']['enq_id']);
+            $getEnquirypaymentInfo= $this->enquiry_model->getEnquirypaymentInfo($data['get_student_enquiry_id']['enq_id'])[0];
+            $gettotalpaidEnquirypaymentInfo = $this->enquiry_model->gettotalpaidEnquirypaymentInfo($data['get_student_enquiry_id']['enq_id']);
 
+
+            if(!empty($gettotalpaidEnquirypaymentInfo[0]->totalpaidAmount)){
+                $totalpaidAmount =  $gettotalpaidEnquirypaymentInfo[0]->totalpaidAmount;
+            }else{
+                $totalpaidAmount = 0;  
+            }  
     
           
-            $payment_details = array('amount_total'=>$followDataenquiry->final_amount,'amount_paid_by_you'=>200,'pending_amount_by_you'=>500);
+            $payment_details = array('amount_total'=>$followDataenquiry->final_amount,'amount_paid_by_you'=>$totalpaidAmount,'pending_amount_by_you'=>500);
             $total_dashbaord_count = array('total_courses'=>$total_course_count,'total_admission'=>$getstudentEnquiryCount,'total_invoices'=>$getTaxinvoicesCountstudent,'total_exam_notification'=>count($examnotification),'total_replay_on_query'=>$getReplyonyourquerystudent,'total_class_request'=>$getClassrequestcountstudent);
            
            
