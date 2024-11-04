5f5c881f9af1463722b65d05cd0a1c92e69ca682
Hemant katurde <hemantkaturde123@gmail.com>
2024-11-04 13:16:22 +0530

    code for added new changes

--- payment/pay.php
+++ payment/pay.php
@@ -221,8 +221,23 @@
     </div>
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://iictn.org.in/admission/js/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"> </script>
+    <script src="https://iictn.org.in/admission/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://iictn.org.in/admission/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="https://iictn.org.in/admission/js/grasp_mobile_progress_circle-1.0.0.min.js"></script>
+    <script src="https://iictn.org.in/admission/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script
+        src="https://iictn.org.in/admission/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js">
+    </script>
+    <script src="https://iictn.org.in/admission/js/charts-home.js"></script>
+    <script src="https://iictn.org.in/admission/js/front.js"></script>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
     
+
     <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
     <script>
         $(".loader_ajax").show();
