22771d18a6088da305c4cfa85dff95434e147e13
Hemant katurde <hemantkaturde123@gmail.com>
2024-11-05 16:35:41 +0530

    code for added new changes

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -997,7 +997,7 @@
 
                         foreach($getSyllabusData as $doc_url)
                         {
-                            $doc_url_val .= '<link'.$i.'>'.$doc_url->doc_url.' ';
+                            $doc_url_val .= '<link>'.$i.')'.$doc_url->doc_url.' ';
                         }
 
                         $i++;  
