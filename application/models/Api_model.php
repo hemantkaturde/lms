7ef9ea75151a68a91ae87344f4be4b9a4e8fb9f5
Hemant katurde <hemantkaturde123@gmail.com>
2024-11-14 18:32:37 +0530

    code for change job work

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -1076,10 +1076,15 @@ class Api_model extends CI_Model
                 foreach ($fetch_result1 as $key => $value)
                 {  
 
+
+                    print_r($userId.'ttt');
+                    exit;
+
+                    print_r($value['course_id']);
+                    exit;
+
                     $getspecailpermision_for_exam = $this->getspecailpermisionforexam($userId,$value['course_id']);
 
-                    print_r($getspecailpermision_for_exam);
-                    exit;
 
    
                     if($peecentage==100){
