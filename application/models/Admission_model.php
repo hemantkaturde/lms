d68b66fff1ed5723d1d9e1b376b21cabc354ec38
Hemant katurde <hemantkaturde123@gmail.com>
2024-11-15 17:03:55 +0530

    code for change job work

--- application/models/Admission_model.php
+++ application/models/Admission_model.php
@@ -1766,7 +1766,7 @@ function studentcertificateData($params)
         $this->db->select('*');
         $this->db->join(TBL_COURSE, TBL_STUDENT_ANSWER_SHEET.'.course_id = '.TBL_COURSE.'.courseId');
         $this->db->join(TBL_EXAMINATION, TBL_STUDENT_ANSWER_SHEET.'.exam_id = '.TBL_EXAMINATION.'.id');
-        $this->db->join(TBL_USER, TBL_STUDENT_ANSWER_SHEET.'.student_id = '.TBL_USER.'.userId');
+       // $this->db->join(TBL_USER, TBL_STUDENT_ANSWER_SHEET.'.student_id = '.TBL_USER.'.userId');
         // $this->db->join(TBL_TIMETABLE_TRANSECTIONS, TBL_TIMETABLE_TRANSECTIONS.'.course_id = '.TBL_COURSE.'.courseId');
 
         // if($params['search']['value'] != "") 
