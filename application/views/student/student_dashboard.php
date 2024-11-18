76b9ba190169643a43ad3f71932c22de79cbe1ae
Hemant katurde <hemantkaturde123@gmail.com>
2024-11-18 11:43:35 +0530

    code for change job work

--- application/views/student/student_dashboard.php
+++ application/views/student/student_dashboard.php
@@ -328,6 +328,7 @@
 							<button id="join_link" class="join_link" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>"  meeting_link="<?=$value['link_url']?>" >JOIN</button>
 						<?php } ?>
 								<button id="attend_manually" class="attend_manually" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Click To Attend</button>
+							    <button id="print_id_card" class="print_id_card" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Print Id Card</button>
                        <?php }else {?>
 						<b>Attended</b>
                      <?php } }else{ ?>
