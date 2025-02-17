2ec4a54ebd5d3b131b5cf09f1e886e5104d03cdd
Hemant <hemantkaturde123@gmail.com>
2025-02-17 15:33:18 +0530

    code for added new changes

---
+++ application/views/reports/inquiryleadsreport.php
@@ -0,0 +1,45 @@
+<?php $roleText = $this->session->userdata('roleText');?>
+<div class="content-wrapper">
+    <!-- <div class="page-heading">
+        <h3 class="page-title">Users Listing</h3>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index.html"><i class="la la-home font-20"></i></a>
+            </li>
+           <li class="breadcrumb-item">Users Listing</li> 
+        </ol>
+    </div> -->
+    <!-- START PAGE CONTENT-->
+    <div class="page-content fade-in-up">
+        <div class="ibox">
+            <div class="ibox-head">
+                <!-- <div class="ibox-tools"> -->
+                    <!-- <a class="btn btn-primary text-white" onclick="enquiry(0)" ><i class="fa fa-plus"></i> Add Enquiry</a> -->
+                    <!-- <a class="ibox-collapse"><i class="fa fa-minus"></i></a> -->
+                <!-- </div> -->
+                <div class="ibox-title">Inquiry Leads Report</div>
+            </div>
+            <div class="ibox-body">
+              <div class="panel-body table-responsive">
+                <table id="view_enquirylist" class="table table-bordered">
+                    <thead>
+                                <tr>
+                                    <th>Inquiry No.</th>
+                                    <th>Inquiry Date</th>
+                                    <th>Name</th>
+                                    <th>Mobile No</th>
+                                    <th>Courses</th>
+                                    <th>Counsellor</th>
+                                    <th>Status</th>
+                                    <!-- <th>Fees</th> -->
+                                    <th>Action</th>
+                                </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                 </table>
+              </div>
+            </div>
+        </div>
+    </div>
+<!-- END PAGE CONTENT-->
