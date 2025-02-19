8a886bf6424b8c1a104ccd74f8f13e2cb2d1fde5
Hemant <hemantkaturde123@gmail.com>
2025-02-19 14:59:49 +0530

    code for added new changes

--- application/views/reports/inquiryleadsreport.php
+++ application/views/reports/inquiryleadsreport.php
@@ -24,7 +24,7 @@
                     <div class="row">
                         <div class="col-sm">
                             <div class="form-group">
-                                <label style="margin-left: -13px;!important" class="col-sm-3 col-form-label"><b>Seacrh </b> <span class="required">*</span></label>
+                                <label style="margin-left: -13px;!important" class="col-sm-12 col-form-label"><b>Seacrh </b> <span class="required">*</span></label>
                                 <input  style="" type="text" class="form-control"  id="assign_date" name="assign_date">
                                 <p class="error assign_team_error"></p>
                             </div>
@@ -32,7 +32,7 @@
 
                         <div class="col-sm">
                            <div class="form-group">
-                                <label style="margin-left: -13px;!important" class="col-sm-3 col-form-label"><b>From Date</b> <span class="required">*</span></label>
+                                <label style="margin-left: -13px;!important" class="col-sm-12 col-form-label"><b>From Date</b> <span class="required">*</span></label>
                                 <input  style="" type="text" class="form-control datepicker" value="<?=$current_date;?>" id="assign_date" name="assign_date">
                                 <p class="error assign_team_error"></p>
                             </div>
@@ -40,14 +40,14 @@
 
                         <div class="col-sm">
                             <div class="form-group">
-                                <label style="margin-left: -13px;!important" class="col-sm-3 col-form-label"><b>To Date</b> <span class="required">*</span></label>
+                                <label style="margin-left: -13px;!important" class="col-sm-12 col-form-label"><b>To Date</b> <span class="required">*</span></label>
                                 <input  style="" type="text" class="form-control datepicker" value="<?=$current_date;?>" id="assign_date" name="assign_date">
                                 <p class="error assign_team_error"></p>
                             </div>
                         </div>
                         <div class="col-sm">
                             <div class="form-group">
-                                <label style="margin-left: -13px;!important" class="col-sm-3 col-form-label"><b>Date</b> <span class="required">*</span></label>
+                                <label style="margin-left: -13px;!important" class="col-sm-12 col-form-label"><b>Date</b> <span class="required">*</span></label>
                                 <input  style="" type="text" class="form-control datepicker" value="<?=$current_date;?>" id="assign_date" name="assign_date">
                                 <p class="error assign_team_error"></p>
                             </div>
