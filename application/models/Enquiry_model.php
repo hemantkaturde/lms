c3a019723f661df3fb7689289cba9f41547134dc
hemant-rioga <hemantkk@riogapremium.com>
2025-02-24 12:24:24 +0530

    code for remove space form search string

--- application/models/Enquiry_model.php
+++ application/models/Enquiry_model.php
@@ -1010,9 +1010,8 @@ public function getaddoncoursepaymentdetailsCount($params,$id){
 
    public function getEnquiryreportCount($params,$search_by_any,$from_date,$to_date){
    
-    $new_string = str_replace(' ', '%20', $search_by_any);
+    $new_string = str_replace('%20', ' ', $search_by_any);
 
-   
     $this->db->select('*');
     // $this->db->join(TBL_COURSE_TYPE, TBL_COURSE_TYPE.'.ct_id = '.TBL_ENQUIRY.'.course_type_id','left');
     $this->db->join(TBL_USER, TBL_USER.'.userId = '.TBL_ENQUIRY.'.counsellor_id');
@@ -1037,10 +1036,8 @@ public function getaddoncoursepaymentdetailsCount($params,$id){
 
 public function getEnquiryreportdata($params,$search_by_any,$from_date,$to_date){
 
-    $new_string = str_replace(' ', '%20', $search_by_any);
+    $new_string = str_replace('%20', ' ', $search_by_any);
 
-    print_r($new_string);
-    exit;
 
     $this->db->select('*,'.TBL_ADMISSION.'.enq_id as admissionexits,'.TBL_ENQUIRY.'.enq_id as enquiry_id,'.TBL_USER.'.name as counseller');
     // $this->db->join(TBL_COURSE_TYPE, TBL_COURSE_TYPE.'.ct_id = '.TBL_COURSE.'.course_type_id','left');
