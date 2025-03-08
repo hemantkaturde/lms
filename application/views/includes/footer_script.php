a0bde7d8bca939b8938470c552c63ab97209107d
Hemant <hemantkk@riogapremium.com>
2025-03-08 17:58:59 +0530

    code for added new changes

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -4831,6 +4831,7 @@ if($pageTitle=='Role Listing' || $pageTitle=='Add New Role' || $pageTitle=='Edit
 	                 { "width": "10%", "targets": 3 },
 	                 { "width": "10%", "targets": 4 },
 					 { "width": "10%", "targets": 5 },
+					 { "width": "10%", "targets": 6},
 	            ],
 	            responsive: true,
 	            "oLanguage": {
