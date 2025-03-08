54a1c6ec3aeb0ff51095acf866c70db7d887e338
Hemant <hemantkk@riogapremium.com>
2025-03-08 16:29:20 +0530

    code for added new changes

--- application/views/student/student_dashboard.php
+++ application/views/student/student_dashboard.php
@@ -322,16 +322,9 @@
                   
                     <?php 
 		
-					if($value['courserestrictId']!==null){ ?>
+					if($value['courserestrictId']==null){ ?>
 
-					<?php $array_of_course_id = json_decode($value['courserestrictId'], true); ?>
-					<?php if (in_array($value['courseId'], $array_of_course_id)) { ?>
-						<td></td>
-						<td><button style="background:grey,color: #ffff"> Please pay the Balance fees. Contact admin</button></td>
-				
-					<?php }else{ ?>
-						<td><?=$value['link_url'] ?></td>
-						<td>
+                        <td>
 							<?php  if($value['iscancle']!=1){ ?>
 							<?php if($value['attendance_alreday_exits']!='1'){ ?>
 								<?php if($value['link_url']){ ?>
@@ -345,24 +338,31 @@
 								<b>Cancelled</b>
 							<?php } ?>
 						</td>
-					<?php } ?>
 
 					<?php }else{  ?>
 
-						<td>
-							<?php  if($value['iscancle']!=1){ ?>
-							<?php if($value['attendance_alreday_exits']!='1'){ ?>
-								<?php if($value['link_url']){ ?>
-									<button id="join_link" class="join_link" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>"  meeting_link="<?=$value['link_url']?>" >JOIN</button>
+						<?php $array_of_course_id = json_decode($value['courserestrictId'], true); ?>
+						<?php if (in_array($value['courseId'], $array_of_course_id)) { ?>
+							<td></td>
+							<td><button style="background:grey,color: #ffff"> Please pay the Balance fees. Contact admin</button></td>
+					
+						<?php }else{ ?>
+							<td><?=$value['link_url'] ?></td>
+							<td>
+								<?php  if($value['iscancle']!=1){ ?>
+								<?php if($value['attendance_alreday_exits']!='1'){ ?>
+									<?php if($value['link_url']){ ?>
+										<button id="join_link" class="join_link" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>"  meeting_link="<?=$value['link_url']?>" >JOIN</button>
+									<?php } ?>
+											<button id="attend_manually" class="attend_manually" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Click To Attend</button>
+											<button id="print_id_card" class="print_id_card" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Print Id Card</button>
+								<?php }else {?>
+									<b>Attended</b>
+								<?php } }else{ ?>
+									<b>Cancelled</b>
 								<?php } ?>
-										<button id="attend_manually" class="attend_manually" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Click To Attend</button>
-										<button id="print_id_card" class="print_id_card" user-id="<?=$value['userid']?>" topic-id="<?=$value['topicid']?>" course-id="<?=$value['courseId']?>" meeting_id="<?=$value['meeting_id']?>">Print Id Card</button>
-							<?php }else {?>
-								<b>Attended</b>
-							<?php } }else{ ?>
-								<b>Cancelled</b>
-							<?php } ?>
-						</td>
+							</td>
+						<?php } ?>
 
 					<?php } ?>
                 </tr>
