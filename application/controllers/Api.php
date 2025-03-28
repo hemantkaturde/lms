12dc17dd750558443debf60c38e18c71acc59d5f
Hemant <hemantkk@riogapremium.com>
2025-03-28 20:50:34 +0530

    code for added new change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,6 +1,9 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 require APPPATH . '/libraries/BaseController.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 class Api extends BaseController
 {
