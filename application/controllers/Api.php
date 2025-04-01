10cb4a58b78af1adb7ff76111af614ec40161ccb
hemant-rioga <hemantkk@riogapremium.com>
2025-04-01 16:51:31 +0530

    code for added new change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,9 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 require APPPATH . '/libraries/BaseController.php';
-header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: ");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
