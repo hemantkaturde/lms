8af4f3b8b35a18958bcc4e2a008c77d5980d0492
hemant-rioga <hemantkk@riogapremium.com>
2025-04-01 17:51:26 +0530

    code for added new change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2,7 +2,7 @@
 
 require APPPATH . '/libraries/BaseController.php';
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, authtoken");
 header("Access-Control-Allow-Credentials: true");
 
