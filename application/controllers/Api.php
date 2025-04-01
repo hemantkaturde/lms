a6b6cd3c2ed4e03e97291b0e80f2d115d7c6ffb6
hemant-rioga <hemantkk@riogapremium.com>
2025-04-01 17:50:07 +0530

    code for added new change

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -3,7 +3,7 @@
 require APPPATH . '/libraries/BaseController.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, authtoken");
 header("Access-Control-Allow-Credentials: true");
 
 class Api extends BaseController
@@ -16,7 +16,7 @@ class Api extends BaseController
 
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, authtoken");
         header("Access-Control-Allow-Credentials: true");
 
          parent::__construct();
